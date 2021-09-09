02c84416a4963b40b5efc0df6d89ec59275f6fc0
LeeJaeMoonND <90170930+LeeJaeMoonND@users.noreply.github.com>
2021-09-09 17:22:15 +0900

    gird.py 세로 축 그리기

--- 04/grid.py
+++ 04/grid.py
@@ -5,8 +5,16 @@ countY=5
 
 while(countY>=0):
     t.penup()
-    t.goto(100,100*countY)
+    t.goto(0,100*countY)
     t.pendown()
     t.forward(500)
     countY=countY-1
-    
+
+t.right(90)
+
+while(countX>=0):
+    t.penup()
+    t.goto(100*countX,500)
+    t.pendown()
+    t.forward(500)
+    countX=countX-1
