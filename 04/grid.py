a86e3a74ca86984b45cb338308a01a9aca4c5060
LeeJaeMoonND <90170930+LeeJaeMoonND@users.noreply.github.com>
2021-09-09 17:25:13 +0900

    gird.py 가운데 정렬

--- 04/grid.py
+++ 04/grid.py
@@ -3,18 +3,23 @@ import turtle as t
 countX=5
 countY=5
 
+#가로 축 그리기
+
 while(countY>=0):
     t.penup()
-    t.goto(0,100*countY)
+    t.goto(-250,(100*countY)-250)
     t.pendown()
     t.forward(500)
     countY=countY-1
 
-t.right(90)
+t.right(90) 
 
+#세로 축 그리기
 while(countX>=0):
     t.penup()
-    t.goto(100*countX,500)
+    t.goto((100*countX)-250,250)
     t.pendown()
     t.forward(500)
     countX=countX-1
+
+t.extionclick() #클릭 전까지 안꺼짐
