c9cd48e5693cd92003ab0cdb3d54964fc60392e9
LeeJaeMoonND <90170930+LeeJaeMoonND@users.noreply.github.com>
2021-09-13 15:07:49 +0900

    dril#05 함수 4 방향으로 수정

--- 05/turtle_move.py
+++ 05/turtle_move.py
@@ -1,12 +1,35 @@
 import turtle
 
-#터틀의 움직임 함수(동쪽 이동 시 radius = 0)
-def turtle_move(radius):
-    turtle.setheading(radius)
+#터틀의 왼쪽 움직임
+def turtle_Left():
+    turtle.setheading(0)
     turtle.forward(50)
     turtle.stamp()
+
+#터틀의 오른쪽 움직임
+def turtle_Right():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+
+#터틀의 상 움직임
+def turtle_Up():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+
+#터틀의 하 움직임
+def turtle_Down():
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
+
 
 #리셋 함수
 def reset_turtle():
     turtle.reset()
+
+
+
+turtle.listen()
 
