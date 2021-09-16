525640962808e64e5292a3fea834d3c98dec6e68
LeeJaeMoonND <90170930+LeeJaeMoonND@users.noreply.github.com>
2021-09-16 15:00:27 +0900

    dril #06 사각형 운동

--- 06/character_move.py
+++ 06/character_move.py
@@ -1,12 +1,45 @@
-import pico2d from *
+from pico2d import *
+import math
+
+os.chdir('c:/2DGP/Lecture04_2D_Rendering')
 
 open_canvas()
 
-grass= load_image('grass.png')
+grass = load_image('grass.png')
 character = load_image('character.png')
 
-x = 0
-y = 0
+x = 400
+y = 90
+
+def move_character(x,y):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,y)
+
+while(True):
+    while x<750:
+        move_character(x,y)
+        x+=2
+        delay(0.01)
+    while y < 510:
+        move_character(x,y)
+        y+=2
+        delay(0.01)
+    while x > 50:
+        move_character(x,y)
+        x-=2
+        delay(0.01)
+    while y > 90:
+        move_character(x,y)
+        y-=2
+        delay(0.01)
+    while x < 400:
+        move_character(x,y)
+        x+=2
+        delay(0.01)
+        
+    
+    
     
 
 
